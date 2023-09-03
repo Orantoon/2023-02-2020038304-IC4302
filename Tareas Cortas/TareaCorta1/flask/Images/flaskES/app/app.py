962c0dcc819924eded47b41110fffe28cfc650aa
Aarón Ortiz <aaronortizjimenez@gmail.com>
2023-09-03 17:55:13 -0600

    Se agrego a elasticsearch las rutas de actualizar, borra y buscar

--- Tareas Cortas/TareaCorta1/flask/Images/flaskES/app/app.py
+++ Tareas Cortas/TareaCorta1/flask/Images/flaskES/app/app.py
@@ -8,34 +8,45 @@ ESPASSWORD= os.getenv('ESPASSWORD')
 app = Flask(__name__)
 es = Elasticsearch("http://"+ESENDPOINT+":9200", basic_auth=("elastic", ESPASSWORD), verify_certs=False)
 print(f"Conectado a elasticsearch: '{es.info().body['cluster_name']}'")
-
-@app.route('/index', methods=['POST'])
+#crear
+@app.route('/crear', methods=['POST'])
 def Crear_Documento():
     data = request.json
     response = es.index(index='prueba', body=data)
     print(response)
     return jsonify(response['_shards'])
-
-@app.route('/task', methods=['GET'])
-def get_jobs():
-    resp =es.search(index='prueba', q='Aaron Ortiz')
+#Buscar
+@app.route('/buscar/<nombre>', methods=['GET'])
+def get_jobs(nombre):
+    resp =es.search(index='prueba', q=nombre)
     print(resp)
-    return "ejecutado"
+    return resp['hits']['hits']
+#Actualizar
+@app.route('/actualizar/<nombre>', methods=['POST'])
+def actualizar(nombre):
+    cambio = {'doc': request.json}
+    print(cambio)
+    resultado = es.search(index='prueba', query={'match':{'ps':f'{nombre}'}})
+    if resultado['hits']['total']['value']>0:
+        indice = resultado['hits']['hits'][0]['_id']
+        result= es.update(index='prueba', id=indice, body=cambio)
+        print(result)
+        return "cambio"    
+#borrar
+@app.route('/delete/<nombre>', methods = ['DELETE'])
+def BorrarDocumento(nombre):
+    resultado = es.search(index='prueba', query={'match':{'ps':f'{nombre}'}})
+    if resultado['hits']['total']['value']>0:
+        indice = resultado['hits']['hits'][0]['_id']
+        result = es.delete(index='prueba', id=indice)
+        print(result)
+        return "Borrado"
+    else:
+        return "No eliminado"
 
 @app.route('/', methods=['GET'])
 def ping():
     return jsonify({"hello": "My name is"})
-
-@app.route('/borrar/<documento_id>', methods=['DELETE'])
-def borrarDoc(documento_id):
-    try:
-        response = es.delete(index='prueba', id= documento_id)
-        if response['result']== 'deleted':
-            return jsonify({'mensaje': f'El documento id: {documento_id} fue borrado'})
-        else:
-            return jsonify({'error': f'El documento id: {documento_id} no fue borrado'})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug= True)

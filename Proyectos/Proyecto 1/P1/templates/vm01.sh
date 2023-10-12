1c69855b024cfa6e09e14c77bbed0f75491f4645
Deif <davidsuarez2001@gmail.com>
2023-10-11 18:55:58 -0600

    Update del Terraform Template
    Se agrego el docker run del loader al terraform template.

--- Proyectos/Proyecto 1/P1/templates/vm01.sh
+++ Proyectos/Proyecto 1/P1/templates/vm01.sh
@@ -18,4 +18,5 @@ chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+docker run -it --name loader_container orantoon/loader
 docker run -d -p 80:5000 --name apimongo aaronortijimn/apimongo

3759784f5cad1989d62e318529b58db9b201e28f
JesusA02 <cortesj02@estudiantec.cr>
2023-10-21 13:17:13 -0600

    mariadb

--- Tareas Cortas/TareaCorta2/TareaCorta2/helm/backups/scripts/mariadb.sh
+++ Tareas Cortas/TareaCorta2/TareaCorta2/helm/backups/scripts/mariadb.sh
@@ -2,10 +2,13 @@
 DATE=$(date '+%Y%m%d%H%M')
 mkdir -p /mariadbdump/$DATE
 
+curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+cat << EOF > /etc/yum.repos.d/
 [mariadb]
 name=MariaDB
 baseurl=https://rpm.mariadb.org/10.6/rhel/$releasever/$basearch
 gpgkey=https://rpm.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
-
-sudo yum install MariaDB-backup #Mariabackup
+EOF
+aws s3 cp /mariadbdump/$DATE s3://$BUCKET_NAME/$BACKUP_PATH/ --recursive
+aws s3 ls s3://$BUCKET_NAME/$BACKUP_PATH/

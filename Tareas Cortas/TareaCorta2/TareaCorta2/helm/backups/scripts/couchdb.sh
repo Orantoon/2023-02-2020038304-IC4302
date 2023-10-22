ddc03e7a5249c38a1b155728eb160fb360c4d3bc
Justin <justingilberto2000@gmail.com>
2023-10-21 22:30:19 -0600

    fix: fixing job and script

--- Tareas Cortas/TareaCorta2/TareaCorta2/helm/backups/scripts/couchdb.sh
+++ Tareas Cortas/TareaCorta2/TareaCorta2/helm/backups/scripts/couchdb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DATE=$(date '+%Y%m%d%H%M')
+BACKUP_DIR="/couchdbdump/$DATE"
+
 mkdir -p /couchdbdump/$DATE
 
 cat <<EOT > /etc/yum.repos.d/apache-couchdb.repo
@@ -13,7 +15,9 @@ EOT
 yum update
 yum install -y couchdb
 mkdir -p $couchdbdump
-curl -X GET "http://localhost:5984/databases-couchdb/_all_docs?include_docs=true" $couchdbdump/backup.json
+
+curl -X GET "$COUCHDB_CONNECTION_STRING/databases-couchdb/_all_docs?include_docs=true" > "$BACKUP_DIR/backup.json"
+
 aws s3 cp /couchdbdump/$DATE s3://$BUCKET_NAME/$BACKUP_PATH/ --recursive
 aws s3 ls s3://$BUCKET_NAME/$BACKUP_PATH/
 rm -rf /couchdbdump/$DATE

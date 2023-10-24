04a5553d54d378cdc08bcd88900245c8d2fc0631
Justin <justingilberto2000@gmail.com>
2023-10-23 19:30:43 -0600

    feat: finishing couch backup

--- Tareas Cortas/TareaCorta2/TareaCorta2/helm/backups/scripts/couchdb.sh
+++ Tareas Cortas/TareaCorta2/TareaCorta2/helm/backups/scripts/couchdb.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 DATE=$(date '+%Y%m%d%H%M')
-BACKUP_DIR="/couchdbdump/$DATE"
 
 mkdir -p /couchdbdump/$DATE
 
+yum install -y yum-utils
+yum-config-manager --add-repo https://couchdb.apache.org/repo/couchdb.repo
 cat <<EOT > /etc/yum.repos.d/apache-couchdb.repo
-[Apache-Couchdb]
-name=Couchdb Repository
-baseurl=https://apache.jfrog.io/artifactory/couchdb-rpm/el$releasever/$basearch/
+[couchdb]
+name=couchdb
+baseurl=https://apache.jfrog.io/artifactory/couchdb-rpm/el9Server/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://couchdb.apache.org/repo/keys.asc https://couchdb.apache.org/repo/rpm-package-key.asc
 EOT
 yum update
-yum install -y couchdb
-mkdir -p $couchdbdump
-
-curl -X GET "$COUCHDB_CONNECTION_STRING/databases-couchdb/_all_docs?include_docs=true" > "$BACKUP_DIR/backup.json"
-
-aws s3 cp /couchdbdump/$DATE s3://$BUCKET_NAME/$BACKUP_PATH/ --recursive
+curl -X GET -u admin:2CU2iFr6m2auqNFJKlvO http://10.1.6.217:5984/couchdb/_all_docs?include_docs=true > /couchdbdump/$DATE/backup.json
+cat /couchdbdump/$DATE/backup.json
+aws s3 cp /couchdbdump/$DATE/ s3://$BUCKET_NAME/$BACKUP_PATH/ --recursive
 aws s3 ls s3://$BUCKET_NAME/$BACKUP_PATH/
 rm -rf /couchdbdump/$DATE

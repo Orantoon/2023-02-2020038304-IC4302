90651805731f684f3d7b95f035db94c1df7e6bc0
joctan04 <joctan@estudiantec.cr>
2023-10-24 20:40:23 -0600

    adding extra settings to postgres

---
+++ Tareas Cortas/TareaCorta2/TareaCorta2/helm/backups/scripts/postgresql-restore.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DATE=$(date '+%Y%m%d%H%M')
+mkdir -p ~/tmp/postgrerestore/$DATE
+
+yum update
+amazon-linux-extras install postgresql14
+
+aws s3 cp s3://tec-ic4302-02-2023/2022437529/postgresql/backup_postgre.sql ~/tmp/postgrerestore/$DATE/backup_postgre.sql
+ls ~/tmp/postgrerestore/$DATE
+pg_restore -d postgreDBRestored ~/tmp/postgrerestore/$DATE/backup_postgre.sql
+
+rm -rf ~/tmp/postgrerestore/$DATE

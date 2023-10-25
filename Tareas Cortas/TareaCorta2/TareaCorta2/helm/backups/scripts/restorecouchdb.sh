8f86c00ab5a2e7b8a43945e908ee32edc26f1ad9
Justin <justingilberto2000@gmail.com>
2023-10-24 20:40:55 -0600

    fix: fixing last bugs in script

--- Tareas Cortas/TareaCorta2/TareaCorta2/helm/backups/scripts/restorecouchdb.sh
+++ Tareas Cortas/TareaCorta2/TareaCorta2/helm/backups/scripts/restorecouchdb.sh
@@ -10,14 +10,13 @@ gpgcheck=1
 enabled=1
 gpgkey=https://couchdb.apache.org/repo/keys.asc https://couchdb.apache.org/repo/rpm-package-key.asc
 EOT
+
 yum update
 yum install jq -y
-jq --version 
-CURRENT_REV=$(curl -s -X GET "$COUCHDB_URL/$DB_NAME/$DOCUMENT_ID" | jq -r '._rev')
 
 aws s3 cp s3://$BUCKET_NAME/$BACKUP_PATH/ /backup/ --recursive
 jq  '{docs: [.rows[].doc]}' ../backup/backup.json > documentos.json
 cat documentos.json
-curl -X POST -d @"documentos.json" -H "Content-Type: application/json" -u admin:$COUCHDB_PASSWORD 10.1.7.17:5984/couchdb/_bulk_docs
+curl -X POST -d @"documentos.json" -H "Content-Type: application/json" -u admin:$COUCHDB_PASSWORD http://databases-couchdb:5984/couchdb/_bulk_docs
 
 rm -rf /backup/

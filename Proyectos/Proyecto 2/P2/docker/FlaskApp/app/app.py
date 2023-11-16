d706990ca02e3099ae26d6a56114d4a9e1e0ab6d
Justin <justingilberto2000@gmail.com>
2023-11-15 18:20:27 -0600

    change port

--- Proyectos/Proyecto 2/P2/docker/FlaskApp/app/app.py
+++ Proyectos/Proyecto 2/P2/docker/FlaskApp/app/app.py
@@ -203,4 +203,4 @@ def directorAsActor(value):
 
 # Main
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0", debug= True)    
+    app.run(port=5001, host="0.0.0.0", debug= True)    

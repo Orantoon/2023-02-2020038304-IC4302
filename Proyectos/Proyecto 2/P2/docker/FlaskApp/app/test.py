fe0726142ab422e3fe49dc792593ddbd4acb5400
joctan04 <joctan@estudiantec.cr>
2023-11-15 23:17:53 -0600

    feat: adding unit test (test_castAsActor, test_castAsDirector, test_DirectorAsDirector, test_directorAsActor)

--- Proyectos/Proyecto 2/P2/docker/FlaskApp/app/test.py
+++ Proyectos/Proyecto 2/P2/docker/FlaskApp/app/test.py
@@ -5,14 +5,41 @@ import app
 from app import *
 import json
 
+URL = 'https://18tkx655-5001.euw.devtunnels.ms/'
+
 class test_api(unittest.TestCase):
     def test_login(self):
-        url = 'https://18tkx655-5001.euw.devtunnels.ms/login'
-        credentials =json.dumps({
-                        "email": "joctan@estudiantec.cr",
-                        "password":" 12345"
-                    })
-        response = requests.post(url,credentials)
+        headers = {
+            'Content-Type': 'application/json', 
+        }
+
+        credentials = json.dumps({
+            "email": "unittest@estudiantec.cr",
+            "password": "unittest"
+        })
+
+        # Proporcionar los encabezados como un parámetro adicional en la solicitud post
+        response = requests.post(URL+"login", data=credentials, headers=headers)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_getMovies(self):
+        response = requests.get(URL+"neo4j/search/Matrix")
+        self.assertEqual(response.status_code, 200)
+
+    def test_castAsActor(self):
+        response = requests.get(URL+"neo4j/castAsActor/Tom Hanks")
+        self.assertEqual(response.status_code, 200)
+
+    def test_castAsDirector(self):
+        response = requests.get(URL+"neo4j/castAsDirector/Tom Hanks")
+        self.assertEqual(response.status_code, 200)
+
+    def test_directorAsDirector(self):
+        response = requests.get(URL+"neo4j/directorAsDirector/Tom Hanks")
+        self.assertEqual(response.status_code, 200)
+
+    def test_directorAsActor(self):
+        response = requests.get(URL+"neo4j/directorAsActor/Tom Hanks")
         self.assertEqual(response.status_code, 200)
 
 
@@ -20,4 +47,3 @@ class test_api(unittest.TestCase):
 
 if __name__=="__main__":
    unittest.main()
-                        

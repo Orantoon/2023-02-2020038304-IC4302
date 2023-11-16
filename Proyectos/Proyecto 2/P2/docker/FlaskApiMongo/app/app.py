bb58cb7bd12112342e0de13d70026a81b5c019c1
joctan04 <joctan@estudiantec.cr>
2023-11-16 02:33:43 -0600

    fix: removing invalid characters

--- Proyectos/Proyecto 2/P2/docker/FlaskApiMongo/app/app.py
+++ Proyectos/Proyecto 2/P2/docker/FlaskApiMongo/app/app.py
@@ -414,7 +414,7 @@ def directorAsActor(value):
             
         print(records)
         
-        return result,200
+        return result,200
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", debug= True)    
